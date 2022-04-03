e3396756eaa0d725fb3874512785dbbe4997ecc4
Aman Bhargava <amanb2000@gmail.com>
2022-04-03 08:07:28 -0400

    Add BCNF checker and decomposition algorithm

--- lib/DesignTools.jl
+++ lib/DesignTools.jl
@@ -1,5 +1,5 @@
 ## Attribute closure algorithm
-function attribute_closure(Y::Set{String}, S::Set)
+function attribute_closure(Y::Set{String}, S::Set; verbose::Bool=false)
     """Given a set of attributes `Y` and some set of functional dependencies
     `S`, returns the closure of attributes `Y`.
     """
@@ -10,10 +10,22 @@ function attribute_closure(Y::Set{String}, S::Set)
         for pair_ in S
             lhs = pair_[1]
             rhs = pair_[2]
+            if verbose
+                println("LHS: ", lhs)
+                println("RHS: ", rhs)
+                println("Y_plus: ", Y_plus)
+            end
             if issubset(lhs, Y_plus)
+                if verbose
+                    println("LHS ", lhs," is a subset of Y_plus ", Y_plus)
+                end
                 if !issubset(rhs,Y_plus)
+                    if verbose
+                        println("RHS ", rhs, " is not in Y_plus! Adding it to Y_plus...")
+                    end
                     did_change = true
                     union!(Y_plus, rhs)
+                    break
                 end
             end
         end
@@ -40,7 +52,7 @@ end
 ## Projection algorithm: Given some set of functional dependencies  and a
  # subset of attributes `L`, we return all the FD's that follow from the
  # original set that ONLY concern attributes in `L`
-function project(S::Set, L::Set{String})
+function project(S::Set, L::Set{String}; verbose::Bool=false)
     """Project the functional dependencies in `S` onto the attributes
     in `L`.
     """
@@ -52,10 +64,17 @@ function project(S::Set, L::Set{String})
     for X in L_powerset
         X_set = Set(X)
         X_set_closure = attribute_closure(X_set, S)
-        println("X_set is ", X_set, " with closure ", X_set_closure)
+
+        if verbose
+            println("X_set is ", X_set, " with closure ", X_set_closure)
+        end
+
         for A in X_set_closure # a is a string representing 1 attribute
             if A in L && !(A in X)
-                println("\tAdding attribute ", A, " because it is in `L` and NOT in `X`")
+                if verbose
+                    println("\tAdding attribute ", A, " because it is in `L` and NOT in `X`")
+                end
+
                 push!(T, Set(X)=>Set([A]))
             end
         end
@@ -70,7 +89,7 @@ end
  # 2. No functional dependencies with unnecessary attributeson the LHS.
  # 3. All RHS are single attributes.
 
-function minimal_basis(S::Set)
+function minimal_basis(S::Set; verbose::Bool=false)
     """Takes `S`, a set of pairs of sets, and returns the minimal basis.
     S has the form Set([
             Set(["A", "B"]) => Set(["C"]),
@@ -88,13 +107,16 @@ function minimal_basis(S::Set)
             push!(S_min, lhs => Set([attribute]))
         end
     end
+    if verbose
+        println("\nAfter splitting all attributes, S_min is: ")
+        for pair in S_min
+            println("\t",pair)
+        end
 
-    println("\nAfter splitting all attributes, S_min is: ")
-    for pair in S_min
-        println("\t",pair)
+        println("\nBeginning step 2: removing LHS attributes where possible.")
     end
 
-    println("\nBeginning step 2: removing LHS attributes where possible.")
+
     # 2: Removing attributes where possible.
     did_remove = true # flag: whether we were able to remove anything.
     while did_remove
@@ -111,16 +133,20 @@ function minimal_basis(S::Set)
                     is_known = new_pair in S_min
 
                     if (!is_known && fd_follows(S_min, new_lhs=>rhs)) # it does follow...
-                        println("\tAble to remove attribute ", remove_this, " from ", lhs=>rhs)
-                        println("\t\tAdding replacement ", new_lhs=>rhs)
+                        if verbose
+                            println("\tAble to remove attribute ", remove_this, " from ", lhs=>rhs)
+                            println("\t\tAdding replacement ", new_lhs=>rhs)
+                        end
                         push!(S_min, new_lhs=>rhs)
                         S_min = setdiff(S_min, Set([lhs=>rhs]))
                         # pop!(S_min, lhs=>rhs)
                         did_remove = true
                         break
                     elseif is_known && fd_follows(S_min, new_lhs=>rhs)
-                        println("\tStronger version of ", lhs=>rhs, " is already known: ", new_lhs=>rhs)
-                        println("\t\tDeleting ", lhs=>rhs)
+                        if verbose
+                            println("\tStronger version of ", lhs=>rhs, " is already known: ", new_lhs=>rhs)
+                            println("\t\tDeleting ", lhs=>rhs)
+                        end
                         pop!(S_min, lhs=>rhs)
                         did_remove = true
                         break
@@ -133,12 +159,15 @@ function minimal_basis(S::Set)
         end
     end
 
-    println("\nAfter removing redundant LHS terms, S_min is: ")
-    for pair in S_min
-        println("\t",pair)
+    if verbose
+        println("\nAfter removing redundant LHS terms, S_min is: ")
+        for pair in S_min
+            println("\t",pair)
+        end
+        println("\nBeginning step 3: removing redundant FD's...")
     end
 
-    println("\nBeginning step 3: removing redundant FD's...")
+
     # 3: Removing redundant FD's
     did_remove = true
     while did_remove
@@ -146,7 +175,9 @@ function minimal_basis(S::Set)
         for pair_ in S_min
             S_min_potential = setdiff(S_min, Set([pair_]))
             if fd_follows(S_min_potential, pair_)
-                println("\tAble to remove ",pair_," without changing FD's!")
+                if verbose
+                    println("\tAble to remove ",pair_," without changing FD's!")
+                end
                 pop!(S_min, pair_)
                 did_remove = true
                 break
@@ -154,4 +185,79 @@ function minimal_basis(S::Set)
         end
     end
     return S_min
+end
+
+## BCNF <==> the closure of each LHS is the set of all attributes for all
+ # LHS ∈ nontrivial_FDs
+function is_BCNF(All_FDs::Set, specific_FDs::Set, attributes::Set; verbose::Bool=false)
+    """Given some set of non-trivial `specific_FDs` and
+    a list of all FDs `All_FDs` to check against, and
+    the list of all attributes in the table, return
+    whether or not the relation is in BCNF.
+    """
+    for pair in specific_FDs
+        lhs = pair[1]
+        rhs = pair[2]
+
+        lhs_closure = attribute_closure(lhs, All_FDs)
+        if attributes != lhs_closure
+            if verbose
+                println("LHS = ", lhs, " with closure ",lhs_closure," is NOT a superkey for the relation = ", attributes,"! Returning false.")
+            end
+            return false
+        else
+            if verbose
+                println("LHS = ", lhs, " is a superkey for the relation, continuing search...")
+            end
+        end
+    end
+    if verbose
+        println("All LHS in FDs are superkeys; Relation must be in BCNF form!")
+    end
+    return true
+end
+
+## BCNF Decomposition: Given a list of attributes and a set of FD's,
+ # we calcualte the BCNF decomposition of R
+function BCNF_decomp(attributes::Set, FDs::Set; verbose::Bool=false)
+    for fd_pair in FDs
+        fd_individual = Set([fd_pair])
+        if(!is_BCNF(FDs, fd_individual, attributes, verbose=false))
+            if verbose
+                println("Violating FD: ", fd_individual)
+            end
+
+            X = fd_pair[1] # LHS of the offending FD
+            Y = fd_pair[2] # RHS of the offending FD
+            # println("X has type ", typeof(X))
+
+            println("\tX: ", X)
+            X_plus = attribute_closure(X, FDs)
+
+            attributes_1 = X_plus
+            attributes_2 = setdiff(attributes, setdiff(X_plus, X))
+
+
+            FDs_1 = project(FDs, attributes_1)
+            FDs_2 = project(FDs, attributes_2)
+
+            if verbose
+                println("Attributes 1: ", X_plus)
+                println("Attributes 2: ", attributes_2)
+            end
+
+            # if length(attributes_1) == 0 && length(attributes_2) != 0
+            #     return BCNF_decomp(attributes_2, FDs_2)
+            # elseif length(attributes_2) == 0 && length(attributes_1) != 0
+            #     return BCNF_decomp(attributes_1, FDs_1)
+            # else
+            #     return vcat(BCNF_decomp(attributes_1, FDs_1), BCNF_decomp(attributes_2, FDs_2))
+            # end
+
+            # Recursive step
+            return vcat(BCNF_decomp(attributes_1, FDs_1), BCNF_decomp(attributes_2, FDs_2))
+
+        end
+    end
+    return [attributes]
 end
